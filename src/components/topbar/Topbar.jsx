2faa750f1f65ec2a6207f11f2e3d24e62cf83896
Shiham Samsudeen <shihams247@gmail.com>
2022-04-26 08:43:14 +0530

    iniital commit with Navbar in place

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,45 @@
+import "./topbar.css";
+import { Chat, Notifications, Person, Search } from "@mui/icons-material";
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">ShihamSocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="Search for freind, post or video"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <img className="topbarImage" src="/assets/person/1.jpeg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
